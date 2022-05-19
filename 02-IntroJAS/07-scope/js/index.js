958e51563f59a49af0d9f071b812bed9cc553f7e
Anahi Aguilar <anahi.amontes@gmail.com>
2022-05-18 20:08:22 -0500

    Scope

---
+++ 02-IntroJAS/07-scope/js/index.js
@@ -0,0 +1,13 @@
+var color = "negro";
+
+function pinturas(){
+    var color = "rosa";
+
+    console.log(color);
+
+    var listones =  "El liston es el color: " + color;
+    console.log(listones);
+}
+
+pinturas(color);
+console.log(color);
