2a94e545982ef9fb7ce878d7d193d0b0fd07625f
Anahi Aguilar <anahi.amontes@gmail.com>
2023-06-28 13:41:07 -0600

    Kata Fronted 1

---
+++ 03-Estructuras/arreglos/ejemplo.js
@@ -0,0 +1,15 @@
+'use strict'
+
+let arregloDeObjetos = [
+    {
+        nombre: "Elba",
+        edad: 23
+    },
+    {
+        nombre: "Armando",
+        edad: 17
+    }
+]
+
+arregloDeObjetos[1].nombre = 'Melissa'
+console.log(arregloDeObjetos[1].nombre);
