8db1b6a445510c95a0b2ad435096f8e6d53e3b1a
Anahi Aguilar <anahi.amontes@gmail.com>
2024-03-18 16:49:18 -0600

    useRef con input autofocus

---
+++ 06-FrontEnd-avanzado/01-react-forms/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import RefDom from './components/RefDom'
+
+function App() {
+  return (
+    <>   
+      <div className="card">
+        <RefDom/>
+      </div>
+    </>
+  )
+}
+
+export default App
